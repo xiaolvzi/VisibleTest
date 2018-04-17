8bb1073d3b7d21659e453ba514dfd617e28b56ae
Joe Lv <joel@wicresoft.com>
2018-04-17 14:28:13 +0800

    first commit

---
+++ App15/App15/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App15
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            bt.Clicked += Bt_Clicked;
+		}
+
+        private void Bt_Clicked(object sender, EventArgs e)
+        {
+            if (!Poll1.IsVisible)
+            {
+                Poll1.IsVisible = true;
+                Poll1.Focus();
+                return;
+            }
+        }
+    }
+}
